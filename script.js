396636c40f0845049d0db3999690ac2aff4ca58d
Doug McGeehan <djmvfb@mst.edu>
2016-10-06 02:29:32 -0500

    added google analytics

--- script.js
+++ script.js
@@ -21,3 +21,12 @@ window.onload = function() {
 	flail();
 	window.setInterval(flail, 250);
 };
+
+/* Google analytics, 'cause I'm watching you */
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-80565060-3', 'auto');
+ga('send', 'pageview');
